c7cc1a8fc54c749fdeab1087a47834758cd949ff
Shumpei Yamamoto <shumpeta.kus@gmail.com>
2023-03-29 01:16:47 +0900

    GridKeyの抽象クラスを定義

---
+++ src/model/GridKey.ts
@@ -0,0 +1,31 @@
+import { IdObject, IdType } from "@/model/IdObject";
+import { PointType } from "@/model/type/PointType";
+
+export abstract class GridKey implements IdObject {
+  private _type: PointType;
+
+  constructor(type: PointType) {
+    this._type = type;
+  }
+
+  abstract get id(): IdType;
+
+  get type() {
+    return this._type;
+  }
+
+  public equals(obj: Object) {
+    if (this === obj) {
+      return true;
+    }
+    if (obj == null) {
+      return false;
+    }
+    if (!(obj instanceof GridKey)) {
+      return false;
+    }
+
+    const other = obj as GridKey;
+    return this.id === other.id;
+  }
+}
