5c9e1cf7dc3e0a09640709d030cc1c778bfbadb2
Shumpei Yamamoto <shumpeta.kus@gmail.com>
2023-04-08 18:53:04 +0900

    数独問題を解く用にアダプターを定義

---
+++ src/algorithm/adapter/ColumnAdapter.ts
@@ -0,0 +1,72 @@
+import RowAdapter from "@/algorithm/adapter/RowAdapter";
+import DancingLinks from "@/algorithm/DancingLinks";
+import { GridKey } from "@/model/GridKey";
+import { GridOption } from "@/model/GridOption";
+import { IdObject } from "@/utility/model/IdObject";
+
+export default class ColumnAdapter implements DancingLinks.Axis<RowAdapter, ColumnAdapter>, IdObject {
+  private col: GridKey;
+
+  private static feasibles: Map<GridKey, GridOption[]>
+
+  constructor(col: GridKey) {
+    this.col = col;
+  }
+
+  public static setFeasibles(feasibles: Map<GridKey, GridOption[]>) {
+    ColumnAdapter.feasibles = feasibles;
+  }
+
+  get gridKey() {
+    return this.col;
+  }
+
+  get id() {
+    return this.gridKey.id;
+  }
+
+  get size() {
+    return ColumnAdapter.feasibles.get(this.gridKey)?.length ?? 0;
+  }
+
+  public clear() {
+    ColumnAdapter.feasibles.delete(this.gridKey);
+  }
+
+  public restore(param: DancingLinks.Node<RowAdapter, ColumnAdapter>[]) {
+    const options: GridOption[] = [];
+    for (const [row, _] of param) {
+      options.push(row.gridOption);
+    }
+    ColumnAdapter.feasibles.set(this.gridKey, options);
+  }
+
+  public *getForwardNodes(): IterableIterator<DancingLinks.Node<RowAdapter, ColumnAdapter>> {
+    const options = ColumnAdapter.feasibles.get(this.gridKey) ?? [];
+    for (const option of options) {
+      yield [new RowAdapter(option), this];
+    }
+  }
+
+  public *getReverseNodes(): IterableIterator<DancingLinks.Node<RowAdapter, ColumnAdapter>> {
+    const reverseList = Array.from(this.getForwardNodes()).reverse();
+    for (const it of reverseList) {
+      yield it;
+    }
+  }
+
+  public equals(obj: Object) {
+    if (this === obj) {
+      return true;
+    }
+    if (obj == null) {
+      return false;
+    }
+    if (!(obj instanceof ColumnAdapter)) {
+      return false;
+    }
+
+    const other = obj as ColumnAdapter;
+    return this.col.equals(other.col);
+  }
+}
---
+++ src/algorithm/adapter/RowAdapter.ts
@@ -0,0 +1,80 @@
+import ColumnAdapter from "@/algorithm/adapter/ColumnAdapter";
+import DancingLinks from "@/algorithm/DancingLinks";
+import { GridBoxNumKey } from "@/model/GridBoxNumKey";
+import { GridColNumKey } from "@/model/GridColNumKey";
+import { GridKey } from "@/model/GridKey";
+import { GridOption } from "@/model/GridOption";
+import { GridRowColKey } from "@/model/GridRowColKey";
+import { GridRowNumKey } from "@/model/GridRowNumKey";
+import { BoxType } from "@/model/type/BoxType";
+
+export default class RowAdapter implements DancingLinks.Axis<RowAdapter, ColumnAdapter> {
+  private row: GridOption;
+
+  private static feasibles: Map<GridKey, GridOption[]>;
+
+  constructor(row: GridOption) {
+    this.row = row;
+  }
+
+  public static setFeasible(feasbiles: Map<GridKey, GridOption[]>) {
+    RowAdapter.feasibles = feasbiles;
+  }
+
+  get gridOption() {
+    return this.row;
+  }
+
+  get size() {
+    return 4;
+  }
+
+  public clear() {
+    for (const [_, col] of this.getForwardNodes()) {
+      const options = (RowAdapter.feasibles.get(col.gridKey) ?? []).filter(
+        (option) => !option.equals(this.gridOption)
+      );
+      RowAdapter.feasibles.set(col.gridKey, options);
+    }
+  }
+
+  public restore(param: DancingLinks.Node<RowAdapter, ColumnAdapter>[]) {
+    for (const [_, col] of param) {
+      const options = RowAdapter.feasibles.get(col.gridKey) ?? [];
+      options.push(this.gridOption);
+      RowAdapter.feasibles.set(col.gridKey, options);
+    }
+  }
+
+  public *getForwardNodes(): IterableIterator<DancingLinks.Node<RowAdapter, ColumnAdapter>> {
+    const { row, col, num } = this.gridOption;
+    yield [this, new ColumnAdapter(new GridRowColKey(row, col))];
+    yield [this, new ColumnAdapter(new GridRowNumKey(row, num))];
+    yield [this, new ColumnAdapter(new GridColNumKey(col, num))];
+    const b = 3;
+    const box = Math.floor(row / b) * b + Math.floor(col / b);
+    yield [this, new ColumnAdapter(new GridBoxNumKey(box as BoxType, num))];
+  }
+
+  public *getReverseNodes(): IterableIterator<DancingLinks.Node<RowAdapter, ColumnAdapter>> {
+    const reverseList = Array.from(this.getForwardNodes()).reverse();
+    for (const it of reverseList) {
+      yield it;
+    }
+  }
+
+  public equals(obj: Object) {
+    if (this === obj) {
+      return true;
+    }
+    if (obj == null) {
+      return false;
+    }
+    if (!(obj instanceof RowAdapter)) {
+      return false;
+    }
+
+    const other = obj as RowAdapter;
+    return this.row.equals(other.row);
+  }
+}
