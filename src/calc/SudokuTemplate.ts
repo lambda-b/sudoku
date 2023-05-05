74e277af42d52381557b3d2447df23d5c9940235
Shumpei Yamamoto <shumpeta.kus@gmail.com>
2023-05-05 22:57:05 +0900

    templateを更新

--- src/calc/SudokuTemplate.ts
+++ src/calc/SudokuTemplate.ts
@@ -5,30 +5,24 @@ import { ColumnAdapter } from "@/calc/adapter/ColumnAdapter";
 import { RowAdapter } from "@/calc/adapter/RowAdapter";
 import { SudokuColumnFactory } from "@/calc/factory/SudokuColumnFactory";
 import { SudokuRowFactory } from "@/calc/factory/SudokuRowFactory";
-import { GridKey } from "@/model/GridKey";
 import { GridOption } from "@/model/GridOption";
 import { ColType, isColType } from "@/model/type/ColType";
 import { RowType, isRowType } from "@/model/type/RowType";
 import { SolutionNumberType, isSolutionNumberType } from "@/model/type/SolutionNumberType";
-import { IdMap } from "@/utility/IdMap";
 
 class SudokuTemplate {
-  private feasibles = new IdMap<GridKey, GridOption[]>();
 
-  private headers = new Set<Column>();
+  private allColumns = SudokuColumnFactory.createAllColumns();
+
+  private covers = SudokuRowFactory.createAllRows(ColumnAdapter.converter(this.allColumns));
+
+  private headers = new Set<Column>(this.allColumns);
 
   private matrix = new Matrix(this.headers);
 
   public setup(data: string) {
-    const allColumns = SudokuColumnFactory.createAllColumns();
-    const covers = SudokuRowFactory.createAllRows(ColumnAdapter.converter(allColumns));
-    for (const row of covers) {
-      for (const { col } of row.getForwardNodes()) {
-        this.headers.add(col);
-      }
-    }
 
-    const optionMapper = RowAdapter.converter(covers);
+    const optionMapper = RowAdapter.converter(this.covers);
 
     const grid = convert(data);
     grid.forEach((row, i) => {
