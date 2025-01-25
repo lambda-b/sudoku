4fc27961a16b1f65eb2d3c1ff36f17f36d5471d8
lambda <shumpeta.kus@gmail.com>
2025-01-26 00:20:28 +0900

    button更新

--- src/components/block/SudokuSolveButton.tsx
+++ src/components/block/SudokuSolveButton.tsx
@@ -1,28 +1,41 @@
 import { cellUpdater, tableState } from "@/base/jotai/cell";
 import SudokuSolver from "@/calc/SudokuSolver";
+import { cx } from "@emotion/css";
 import { useAtomValue, useSetAtom } from "jotai";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export const SudokuSolveButton = () => {
-
   const data = useAtomValue(tableState);
 
   const setCell = useSetAtom(cellUpdater);
 
   const [solver] = useState<SudokuSolver>(new SudokuSolver(setCell));
 
-  const onClickSolve = () => {
-    const itr = solver.solve(data);
+  const [processing, setProcessing] = useState<boolean>(false);
 
-    const exe = setInterval(() => {
-      if (itr.next().done) {
-        clearInterval(exe);
-      }
-    }, 100);
-  };
+  useEffect(() => {
+    if (processing) {
+      const itr = solver.solve(data);
 
-  return <button
-    className="button"
-    onClick={onClickSolve}
-  >Solve</button>;
+      const exe = setInterval(() => {
+        if (itr.next().done) {
+          clearInterval(exe);
+        }
+      }, 100);
+
+      return () => clearInterval(exe);
+    }
+  }, [processing]);
+
+  return (
+    <button
+      className={cx(
+        "button is-outlined",
+        processing ? "is-danger" : "is-primary"
+      )}
+      onClick={() => setProcessing((b) => !b)}
+    >
+      {processing ? "Stop" : "Solve"}
+    </button>
+  );
 };
